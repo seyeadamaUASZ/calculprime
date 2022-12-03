5960684776068d8d14eab55ddc1f256800c7af55
seyeadamaUASZ <a.seye3777@zig.univ.sn>
2022-12-03 22:22:06 +0100

    calcul

---
+++ src/main/java/com/sid/gl/CalculprimeApplication.java
@@ -0,0 +1,13 @@
+package com.sid.gl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculprimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculprimeApplication.class, args);
+	}
+
+}
